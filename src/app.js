6cde9eb326606deceaf70a030ca644e3c7cae660
sandeep0814 <sandeep0814@gmail.com>
2018-06-05 23:46:08 +0530

    react router setup

--- src/app.js
+++ src/app.js
@@ -1,6 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { BrowserRouter,Route } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/style.scss';
-ReactDOM.render(<p>Boilerplate</p>, document.getElementById('app'));
+
+const expensehome = () => (
+<div>
+    Home page
+    </div>
+);
+
+const addexpense = () => (
+    <div>
+    Add neew expense
+    </div>
+);
+
+const editexpense = () => (
+    <div>
+        Edit your expense
+    </div>
+);
+
+const helpexpense = () => (
+    <div>
+    Help
+    </div>
+);
+
+const routes = (
+    <BrowserRouter>
+    <div>
+    <Route path="/" component={expensehome} exact={true}/>
+    <Route path="/create" component={addexpense} />
+    <Route path="/edit" component={editexpense} />
+    <Route path="/help" component={helpexpense} />
+    </div>
+    </BrowserRouter>
+);
+ReactDOM.render(routes, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,7 @@ module.exports = {
   devServer:{
     contentBase:path.join(__dirname, 'public'),
     inline:true,
-    port:3000
+    port:3000,
+    historyApiFallback:true
   }
 };
