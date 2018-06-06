d08afcaddb7567405c6699b79146e9b9e5ed6001
sandeep0814 <sandeep0814@gmail.com>
2018-06-06 23:13:04 +0530

    client side routing using Navlink switch and route

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter,Route } from 'react-router-dom';
+import { BrowserRouter,Route,Switch,Link,NavLink } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/style.scss';
 
@@ -27,14 +27,33 @@ const helpexpense = () => (
     Help
     </div>
 );
+const notfound = () => (
+    <div>
+    <Link to="/">404. Not found.</Link>
+    </div>
+);
+
+const Header = () => (
+<header>
+    <h1>Expensify</h1>
+    <NavLink to="/" activeClassName="is-active" exact={true}>Home</NavLink>
+    <NavLink to="/create" activeClassName="is-active">Create</NavLink>
+    <NavLink to="/edit" activeClassName="is-active">Edit</NavLink>
+    <NavLink to="/help" activeClassName="is-active">Help</NavLink>
+</header>
+);
 
 const routes = (
     <BrowserRouter>
     <div>
+        <Route component={Header} />
+    <Switch>
     <Route path="/" component={expensehome} exact={true}/>
     <Route path="/create" component={addexpense} />
     <Route path="/edit" component={editexpense} />
     <Route path="/help" component={helpexpense} />
+    <Route component={notfound} />
+    </Switch>
     </div>
     </BrowserRouter>
 );
