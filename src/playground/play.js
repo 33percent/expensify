ae93ff5445d9a19cd58b1c3eb6eb6c9688b2f370
sandeep0814 <sandeep0814@gmail.com>
2018-06-10 12:39:29 +0530

    redux action

--- src/playground/play.js
+++ src/playground/play.js
@@ -1,8 +1,40 @@
 import {createStore} from 'redux';
 
-const store = createStore((state = {count:0})=> {
-    state.count = state.count+1;
-    return state; 
+const store = createStore((state = {count:0},action)=> {
+switch(action.type) {
+    case 'INCREMENT':
+    const incrementby = (typeof action.incrementBy ==='number' ? action.incrementBy : 1);
+    return {
+        count:state.count+incrementby
+    }
+    case 'DECREMENT':
+    const decrementby = (typeof action.decrementBy === 'number' ? action.decrementBy : 1);
+    return {
+        count:state.count-decrementby
+    }
+    case 'RESET':
+    return {
+        count:0
+    }
+    default: 
+    return state;
+}
 });
 
-console.log(store.getState())
+const subscribbe = store.subscribe(()=> {
+    console.log(store.getState());
+});
+
+store.dispatch({
+    type:'INCREMENT',
+    incrementBy:5
+});
+
+store.dispatch({
+    type:'DECREMENT',
+    decrementBy : 5
+});
+
+store.dispatch({
+    type:'RESET'
+})
