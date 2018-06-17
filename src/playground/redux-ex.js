109574f8b065da01f04c9f0102a4aebe89a21da6
sandeep0814 <sandeep0814@gmail.com>
2018-06-17 10:13:03 +0530

    multiple reducers

--- src/playground/redux-ex.js
+++ src/playground/redux-ex.js
@@ -1,19 +1,54 @@
-import {createStore, combineReducers} from "redux";
+import {
+    createStore,
+    combineReducers
+} from "redux";
 
 const demostate = {
-    expenses:[{
-        id:"myone",
-        description:"January Rent",
-        note:"My final payment",
-        amount:54500,
-        createdAt:0
+    expenses: [{
+        id: "myone",
+        description: "January Rent",
+        note: "My final payment",
+        amount: 54500,
+        createdAt: 0
     }],
-    filter:{
-        text:"rent",
-        sortBy:"amount",
-        startDate:undefined,
-        endDate:undefined
+    filter: {
+        text: "rent",
+        sortBy: "amount",
+        startDate: undefined,
+        endDate: undefined
     }
 };
 
-console.log(demostate)
+
+//expenses reducer
+
+const expensesReducerDefault = []
+
+const expensesReducer = (state = expensesReducerDefault, action) => {
+    switch (action.type) {
+        default: return state;
+    }
+};
+
+//Filter reducer;
+
+const filterReducerDefault = {
+    text: "",
+    sortBy: "amount",
+    startDate: undefined,
+    endDate: undefined
+}
+const filterReducer = (state = filterReducerDefault, action) => {
+    switch (action.type) {
+        default: return state;
+    }
+}
+
+const store = createStore(
+    combineReducers({
+        expenses: expensesReducer,
+        filter: filterReducer
+    })
+);
+
+console.log(store.getState());
