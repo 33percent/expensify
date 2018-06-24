88626bb74869260cbd6846517b45f2604b66f13d
sandeep0814 <sandeep0814@gmail.com>
2018-06-24 09:50:29 +0530

    redux srtore setup

--- src/app.js
+++ src/app.js
@@ -3,5 +3,13 @@ import ReactDOM from 'react-dom';
 import 'normalize.css/normalize.css';
 import './styles/style.scss';
 import AppRouter from './routers/AppRouter';
+import configurewStore from './store/configureStore';
+
+import  { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
+
+const store = configurewStore();
+console.log(store.getState());
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
