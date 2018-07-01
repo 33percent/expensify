7f8b22b65cf8bd8d0f2ebb5ba447c3e1357574ed
sandeep0814 <sandeep0814@gmail.com>
2018-07-01 11:29:41 +0530

    connecting dispatch with components

--- src/app.js
+++ src/app.js
@@ -11,22 +11,22 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water bill' }));
+store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }));
 store.dispatch(addExpense({ description: 'Gas bill' }));
 store.dispatch(setTextFilter('water'));
+
+setTimeout(() => {
+  store.dispatch(setTextFilter('bill'));
+}, 3000)
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 console.log(visibleExpenses);
 
-setTimeout(function(){
-store.dispatch(setTextFilter('bill'))
-},3000)
-
-
 const jsx = (
-    <Provider store={store}>
-        <AppRouter />
-    </Provider>
-)
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+);
+
 ReactDOM.render(jsx, document.getElementById('app'));
